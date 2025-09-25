82d97789f19887be7ed75b6fcf3f7d52a05d25e6
Bhanu Teja <bhanutejakokkonda@gmail.com>
2025-09-26 00:54:39 +0530

    added the MONGODB URL using CONFIG SERVICE

--- flexy-management-backend/src/app.module.ts
+++ flexy-management-backend/src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HoardingsModule } from './hoardings/hoardings.module';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [HoardingsModule, CloudinaryModule],
+  imports: [
+    ConfigModule.forRoot({
+       isGlobal: true,
+      envFilePath: '.env.local',
+    }),
+   MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    HoardingsModule, CloudinaryModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- flexy-management-backend/src/main.ts
+++ flexy-management-backend/src/main.ts
@@ -10,6 +10,12 @@ async function bootstrap() {
     whitelist: true,
     transform: true,
   }));
+
+  app.enableCors({
+      origin: 'http://localhost:3000', 
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+  });
   
   // Swagger Config
   const config = new DocumentBuilder()
